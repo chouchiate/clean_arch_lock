4328b695e535e53541fb74d2a336f8450066b9f3
Derick Chou <derick@jubo.health>
2020-12-19 07:46:51 +0800

    Add lanuch.json add all test. Complete get_concrete_number_trivia implementation

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,7 +1,19 @@
+import 'package:clean_arch_lock/core/error/failures.dart';
+import 'package:clean_arch_lock/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:clean_arch_lock/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+
+import '../repositories/number_trivia_repository.dart';
 
 class GetConcreteNumberTrivia {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({
+    @required int number,
+  }) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
 }
--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -27,8 +27,11 @@ void main() {
       when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
           .thenAnswer((_) async => Right(tNumberTrivia));
       // act
-
+      final result = await usecase.execute(number: tNumber);
       // assert
+      expect(result, Right(tNumberTrivia));
+      verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+      verifyNoMoreInteractions(mockNumberTriviaRepository);
     },
   );
 }
