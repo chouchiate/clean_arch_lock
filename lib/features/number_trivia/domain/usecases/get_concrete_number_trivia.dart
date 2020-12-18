018426af7a58c6f868027409fce1298924ec07cf
Derick Chou <derick@jubo.health>
2020-12-19 06:15:05 +0800

    Add implementation and test for get_concrete_number_trivia

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,7 @@
+import 'package:clean_arch_lock/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia(this.repository);
+}
